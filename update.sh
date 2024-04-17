c900f8f06e96cfeb680522d33928b1c18dbc6509
Marius Sutara <33678023+msutara@users.noreply.github.com>
2024-04-17 07:42:18 -0700

    Update update.sh
    do stretch right away

--- update.sh
+++ update.sh
@@ -13,7 +13,7 @@ function ctrl_c() {
 
 if [ `head -1 /etc/apt/sources.list | cut -d' ' -f3` == "jessie" ]; then
   if [ `cat /etc/apt/sources.list | egrep "^deb|^#" | wc -l` -le 4 ]; then
-    echo "# jessie" >> /etc/apt/sources.list
+    echo "# stretch" >> /etc/apt/sources.list
   fi
 fi
 
@@ -21,38 +21,21 @@ DEBIAN_FRONTEND=noninteractive
 state="`tail -1 /etc/apt/sources.list | cut -d' ' -f2 | egrep -v 'http'`"
 
 #
-# refresh jessie aptitude
-#
-jessie () {
-rm -rfv /etc/apt/sources.list.d/*
-dpkg-reconfigure dash #Select NO Here
-lsb_release -a
-cat << EOF > /etc/apt/sources.list
-deb https://archive.debian.org/debian/ jessie main contrib non-free
-deb https://archive.debian.org/debian-security/ jessie/updates main contrib non-free
-EOF
-apt-key update
-apt update
-apt -y upgrade
-echo "# stretch" >> /etc/apt/sources.list
-apt-get -y --purge autoremove
-apt -y purge cloudkey-webui  cloudkey-mtk7623-base-files  
-apt -y purge ubnt-archive-keyring  ubnt-crash-report  ubnt-freeradius-setup  ubnt-mtk-initramfs  ubnt-unifi-setup  unifi
-apt -y purge mongodb-clients  mongodb-server
-apt -y purge postgresql  postgresql-client  postgresql-common  postgresql-contrib
-apt -y purge nginx-common  nginx-light  libnginx-mod-http-echo  
-reboot
-}
-
-#
-# get stretch aptitude
+# STRETCH
 #
 stretch () {
+rm -rfv /etc/apt/sources.list.d/*
 lsb_release -a
 cat << EOF > /etc/apt/sources.list
 deb https://archive.debian.org/debian/ stretch main contrib non-free
 deb https://archive.debian.org/debian-security/ stretch/updates main contrib non-free
 EOF
+apt -y purge  cloudkey-webui  ubnt-archive-keyring  ubnt-crash-report  ubnt-freeradius-setup  ubnt-mtk-initramfs  ubnt-unifi-setup  unifi  ubnt-systemhub
+apt -y purge  mongodb-clients  mongodb-server
+apt -y purge  postgresql  postgresql-client  postgresql-common  postgresql-contrib
+apt -y purge  nginx-common  nginx-light  libnginx-mod-http-echo  
+apt update
+apt-get install debian-archive-keyring
 apt-key update
 apt update
 apt -y upgrade
