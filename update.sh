719dcfc007eae729c34d4675f53f5455a351c786
Marius Sutara <33678023+msutara@users.noreply.github.com>
2024-05-11 16:22:46 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -45,7 +45,6 @@ reboot
 DEBIAN_FRONTEND=noninteractive
 echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 apt -qy purge  debian-archive-keyring  cloudkey-webui  ubnt-archive-keyring  ubnt-crash-report  ubnt-unifi-setup  ubnt-systemhub  nginx-light  libnginx-mod-http-echo  postgresql  postgresql-client  postgresql-common  postgresql-contrib  unifi  mongodb-clients
-rm -rf /var/log/unifi/
 rm -rf /var/www/html/
 apt -qy purge  nginx-common
 apt-get -qy --purge autoremove
@@ -90,7 +89,6 @@ apt-get -qy update
 apt-get -qy -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" upgrade
 apt-get -qy --purge autoremove
 apt-get -qy -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" dist-upgrade 
-echo "# bullseye" >> /etc/apt/sources.list
 reboot
 DEBIAN_FRONTEND=noninteractive
 echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
@@ -100,6 +98,8 @@ apt-get -qy autoclean
 
 if [ -z $state ]; then
   echo "Latest tested version installed..."
+  apt -qy purge  unifi
+  rm -rf /var/log/unifi/
 else
   echo "Starting with $state"
   $state
