1df423acd841a450bd40df1bcfd7a180e5c909f4
Marius Sutara <33678023+msutara@users.noreply.github.com>
2024-05-11 16:37:21 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -17,8 +17,14 @@ if [ `head -1 /etc/apt/sources.list | cut -d' ' -f3` == "jessie" ]; then
   fi
 fi
 
+rm -rfv /etc/apt/sources.list.d/*
+lsb_release -a
+
 DEBIAN_FRONTEND=noninteractive
 echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+
+apt-get -qy --purge autoremove
+apt-get -qy autoclean
 
 state="`tail -1 /etc/apt/sources.list | cut -d' ' -f2 | egrep -v 'http'`"
 
@@ -26,13 +32,14 @@ state="`tail -1 /etc/apt/sources.list | cut -d' ' -f2 | egrep -v 'http'`"
 # STRETCH
 #
 stretch () {
+
 apt -qy purge  ubnt-freeradius-setup  libfreeradius2  freeradius-utils  freeradius-ldap  freeradius-common  freeradius
-rm -rfv /etc/apt/sources.list.d/*
-lsb_release -a
+
 cat << EOF > /etc/apt/sources.list
 deb https://archive.debian.org/debian/ stretch main contrib non-free
 deb https://archive.debian.org/debian-security/ stretch/updates main contrib non-free
 EOF
+
 apt-get -qy update
 apt-get install debian-archive-keyring
 apt-key update
@@ -42,64 +49,53 @@ apt-get -qy --purge autoremove
 apt-get -qy -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" dist-upgrade 
 echo "# buster" >> /etc/apt/sources.list
 reboot
-DEBIAN_FRONTEND=noninteractive
-echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
-apt -qy purge  debian-archive-keyring  cloudkey-webui  ubnt-archive-keyring  ubnt-crash-report  ubnt-unifi-setup  ubnt-systemhub  nginx-light  libnginx-mod-http-echo  postgresql  postgresql-client  postgresql-common  postgresql-contrib  unifi  mongodb-clients
-rm -rf /var/www/html/
-apt -qy purge  nginx-common
-apt-get -qy --purge autoremove
-apt-get -qy autoclean
 }
 
 #
 # BUSTER
 #
 buster () {
-rm -rfv /etc/apt/sources.list.d/*
-lsb_release -a
+
+apt -qy purge  debian-archive-keyring  cloudkey-webui  ubnt-archive-keyring  ubnt-crash-report  ubnt-unifi-setup  ubnt-systemhub  nginx-light  libnginx-mod-http-echo  postgresql  postgresql-client  postgresql-common  postgresql-contrib  unifi  mongodb-clients
+rm -rf /var/www/html/
+apt -qy purge  nginx-common
+apt -qy purge  unifi
+rm -rf /var/log/unifi/
+
 cat << EOF > /etc/apt/sources.list
 deb https://deb.debian.org/debian/ buster main contrib non-free
 deb https://deb.debian.org/debian/ buster-updates main contrib non-free
 deb https://deb.debian.org/debian-security/ buster/updates main contrib non-free
 EOF
+
 apt-get -qy update
 apt-get -qy -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" upgrade
 apt-get -qy --purge autoremove
 apt-get -qy -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" dist-upgrade 
 echo "# bullseye" >> /etc/apt/sources.list
 reboot
-DEBIAN_FRONTEND=noninteractive
-echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
-apt-get -qy --purge autoremove
-apt-get -qy autoclean
 }
 
 #
 # BULLSEYE
 #
 bullseye () {
-rm -rfv /etc/apt/sources.list.d/*
-lsb_release -a
+
 cat << EOF > /etc/apt/sources.list
 deb https://deb.debian.org/debian/ bullseye main contrib non-free
 deb https://deb.debian.org/debian/ bullseye-updates main contrib non-free
 deb https://deb.debian.org/debian-security/ bullseye/updates main contrib non-free
 EOF
+
 apt-get -qy update
 apt-get -qy -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" upgrade
 apt-get -qy --purge autoremove
 apt-get -qy -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" dist-upgrade 
 reboot
-DEBIAN_FRONTEND=noninteractive
-echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
-apt-get -qy --purge autoremove
-apt-get -qy autoclean
 }
 
 if [ -z $state ]; then
   echo "Latest tested version installed..."
-  apt -qy purge  unifi
-  rm -rf /var/log/unifi/
 else
   echo "Starting with $state"
   $state
